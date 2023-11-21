122620207a72849f04f0b1abf360c1686b5fff07
BerkeAnna <berke.anna0803@gmail.com>
2023-11-21 13:38:00 +0100

    login

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -8,13 +8,21 @@ describe('To test the login functionality', () => {
     })
 
         
-        it('sign up to the weather app', () => {
+       /* it('sign up to the weather app', () => {
             cy.get('.signup-link').click();
             cy.get('input[name="email"]').type('testtesttest2@gmail.com');
             cy.get('input[name="email"]').should('have.value', 'testtesttest2@gmail.com');
             cy.get('input[name="password"]').type('test123');
             cy.get('input[name="password"]').should('have.value', 'test123');
             cy.get('.signup-btn').click();
+        });    
+*/
+        it('login to the weather app', () => {
+            cy.get('input[name="email"]').type('testtesttest2@gmail.com');
+            cy.get('input[name="email"]').should('have.value', 'testtesttest2@gmail.com');
+            cy.get('input[name="password"]').type('test123');
+            cy.get('input[name="password"]').should('have.value', 'test123');
+            cy.get('.login-btn').click();
         });    
 
        
