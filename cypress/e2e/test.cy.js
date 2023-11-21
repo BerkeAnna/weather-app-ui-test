7911a858efb9f321458ae793341070c8fffe21b3
BerkeAnna <berke.anna0803@gmail.com>
2023-11-21 13:32:17 +0100

    sign up - not deleted

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,16 +1,23 @@
 //An example test
 describe('To test the login functionality', () => {
     
+    context( 'Sign up' , () => {
     beforeEach('create from', () => {
         cy.visit('https://weather.casrd.de/login');
         cy.wait(5);
-        
     })
 
-
-        it('sign up to the weather app', () => {
-        cy.get('.signup-link').click();
         
-        });
+        it('sign up to the weather app', () => {
+            cy.get('.signup-link').click();
+            cy.get('input[name="email"]').type('testtesttest2@gmail.com');
+            cy.get('input[name="email"]').should('have.value', 'testtesttest2@gmail.com');
+            cy.get('input[name="password"]').type('test123');
+            cy.get('input[name="password"]').should('have.value', 'test123');
+            cy.get('.signup-btn').click();
+        });    
+
+       
+    });
    
 });
