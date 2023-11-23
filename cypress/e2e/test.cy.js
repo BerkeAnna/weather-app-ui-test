c803111d8ac29534c0ec0fe6f060725bea582971
BerkeAnna <berke.anna0803@gmail.com>
2023-11-23 18:12:13 +0100

    search-city-btn click error!

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -8,8 +8,8 @@ describe('To test the login functionality', () => {
     })
 
         it('login to the weather app', () => {
-            cy.get('input[name="email"]').type('testtesttest2@gmail.com');
-            cy.get('input[name="email"]').should('have.value', 'testtesttest2@gmail.com');
+            cy.get('input[name="email"]').type('anna@test.com');
+            cy.get('input[name="email"]').should('have.value', 'anna@test.com');
             cy.get('input[name="password"]').type('test123');
             cy.get('input[name="password"]').should('have.value', 'test123');
             cy.get('.login-btn').click();
@@ -22,7 +22,7 @@ describe('To test the login functionality', () => {
         beforeEach('create from', () => {
             cy.visit('https://weather.casrd.de/login');
             cy.wait(5);
-            cy.loginAs('testtesttest2@gmail.com', 'test123');
+            cy.loginAs('anna@test.com', 'test123');
         })
 
       
@@ -31,7 +31,7 @@ describe('To test the login functionality', () => {
             cy.wait(2);
             cy.get('#Menu_Burger_Icon').should('exist');
             cy.get('#Menu_Burger_Icon').click();
-            cy.get('.account-details .name__text').should('have.text', 'testtesttest2@gmail.com');
+            cy.get('.account-details .name__text').should('have.text', 'anna@test.com');
             cy.get('.account-details .email__text').should('have.text', 'Free Plan');
 
             cy.get('.menu-link').should('contain', 'Home');
@@ -45,7 +45,8 @@ describe('To test the login functionality', () => {
         it('Dark mode button', () => {
             cy.get('.mode-toggle__input').should('exist');
             cy.get('.mode-toggle__circle').click();
-
+            cy.get('header').should('have.css', 'background-color').and('eq', 'rgb(43, 36, 77)');
+            cy.get('header .date__text').should('have.css', 'color').and('eq', 'rgb(255, 255, 255)');
         });
       
 
@@ -107,17 +108,17 @@ describe('To test the login functionality', () => {
             cy.get('.search-city-btn').should('exist');
             cy.wait(50);
             cy.get('.search-city-btn').click();
-            cy.wait(5000);
+            cy.wait(50);
             cy.get('.add-city-btn').should('exist');
             cy.get('.add-city-btn').click();
-            //cy.wait(1);
+            
             cy.get('body').should('contain', 'City has been successfully added!')
             cy.get('#Menu_Burger_Icon').should('exist');
             cy.get('#Menu_Burger_Icon').click();
             cy.wait(4);
             cy.get('li.menu-link').contains('Home').click();
             cy.contains('.weather__card .city-name__text', name).first().should('contain', name);
-            //cy.get('.remove-button').click();
+            cy.get('.remove-button').click();
         });
 
    
